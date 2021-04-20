3d2ac6da877d9df2927fdcfa18b679909c59df55
yifan626 <77653674+yifan626@users.noreply.github.com>
2021-04-20 14:24:30 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,30 @@
+# importing necessary libraries and functions
 import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
 
-app = Flask(__name__)
-model = pickle.load(open('model.pkl', 'rb'))
+app = Flask(__name__)  # Initialize the flask App
+model = pickle.load(open('/Users/yifanli/Downloads/model.pkl', 'rb'))
 
-@app.route('/')
+
+@app.route('/')  # Homepage
 def home():
     return render_template('index.html')
 
-@app.route('/predict',methods=['POST'])
+
+@app.route('/predict', methods=['POST'])
 def predict():
     '''
     For rendering results on HTML GUI
     '''
-    int_features = [int(x) for x in request.form.values()]
-    final_features = [np.array(int_features)]
-    prediction = model.predict(final_features)
 
-    output = round(prediction[0], 2)
+    # retrieving values from form
+    init_features = [float(x) for x in request.form.values()]
+    final_features = [np.array(init_features)]
 
-    return render_template('index.html', prediction_text='Prediction should be $ {}'.format(output))
+    prediction = model.predict(final_features)  # making prediction
+
+    return render_template('output.html',vairaty=prediction)  # rendering the predicted result
 
 
 if __name__ == "__main__":
